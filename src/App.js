48e2452d8662f1567235fc0306ac9ad8356cddc6
Дюша <58334576+Menxy@users.noreply.github.com>
2023-10-07 14:25:34 +0300

    Social-prime 07/10/23

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React from 'react';
-import './App.css';
-import Header from "./components/Header";
-import Nav from "./components/Nav";
-import Profile from "./components/Profile";
+import './App.module.css';
+import Header from "./components/Header/Header";
+import classes from './App.module.css'
+import Content from "./components/Content/Content";
 
 function App() {
     return (
-        <div className={'app-wrapper'}>
+        <div className={classes.app_wrapper}>
             <Header/>
-            <div className={'content'}>
-                <Nav/>
-                <Profile/>
-            </div>
+            <Content/>
         </div>
     );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-
+import classes from './Header.module.css'
 const Header = () => {
-    return <header className={'header'}>
+    return <header className={classes.header}>
         <img
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Official_b.a.t.m.a.n._logo.svg/2560px-Official_b.a.t.m.a.n._logo.svg.png"
             alt="mylogo"/>
